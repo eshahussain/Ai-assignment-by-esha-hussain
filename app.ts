4b053d7205735b32c9b420dec2b4c4d4769ca018
eshahussain <161880229+eshahussain@users.noreply.github.com>
2024-03-02 00:10:54 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,9 @@
+let personName:string = '';
+ personName = prompt('what is youur name?')||'';
+ if (personName !==null && personName !== ''){
+    alert(`hello ${personName}, would you like to learn some python today? `)
+ }
+ else{
+    alert('you have to fill your name!')
+ }
+ 
